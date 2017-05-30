c8095d20136eaaf358e28b9cae8ccd1a18f7c99c
Sean <sevensidedsilver@gmail.com>
2017-05-30 14:21:21 -0600

    complete

--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -3,13 +3,13 @@
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
     messageService.getMessages().then(function ( response ) {
-      $scope.messages = response.data.messages;
+      $scope.messages = response.data.message;
     });
 
     $scope.addMessage = function ( message ) {
       if (message) {
         messageService.addMessage(message).then(function ( response ) {
-          $scope.messages = response.data.messages;
+          $scope.messages = response.data.message;
         });
       }
     };
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 
+var app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [];
+
+app.get('/messages', function (req, res, next){
+  res.status(200).json({message: messages})
+})
+
+app.post('/messages', function (req, res, next){
+  var p = new Date()
+  messages.push({message: req.body.message, time: p.getHours() + ':' + p.getMinutes()});
+  res.status(200).json({message: messages})
+})
+
+app.listen(3000)
